5fb52d78d81ad2fee7fac71f800899ef8178b783
James Gaehring <gaehj457@gmail.com>
2017-06-27 14:40:43 -0400

    mirroring main endpoints from Nik's API on localhost API

--- server/marketday.js
+++ server/marketday.js
@@ -3,11 +3,14 @@ const _ = require('underscore');
 
 const baseURL = 'http://unsqgreenmarket.grownyc.org/api/v1';
 
-const marketProto = {
-  callAPI: function callAPI(endpoint) {
+const apiProto = {
+  callAPI: function callAPI(endpoint, params) {
     return new Promise( function(resolve, reject) {
       let url = baseURL + endpoint;
-      console.log(url);
+      if (params) {
+        url = url + params
+      }
+      console.log("Request sent to " + url + " ...");
       http.get(url, function(res) {
         let body = '';
         res.on('data', function(chunk){
@@ -15,7 +18,7 @@ const marketProto = {
         });
         res.on('end', function(){
           body = JSON.parse(body);
-          console.log("Got a from response from the marketday API");
+          console.log("Got a response from the marketday API");
           resolve(body);
         });
       }).on('error', function(e){
@@ -25,10 +28,10 @@ const marketProto = {
   },
 }
 
-const marketFactory = function marketFactory(options) {
-    return _.extend(Object.create(marketProto), options)
+const apiFactory = function apiFactory(options) {
+    return _.extend(Object.create(apiProto), options)
 };
 
-let marketday = marketFactory()
+let api = apiFactory()
 
-module.exports = marketday;
+module.exports = api;
