6952cd12c654cd23e9a9b73a5b2cbb9866261abd
James Gaehring <gaehj457@gmail.com>
2017-06-28 21:29:10 -0400

    adding slick carousel for seasonal products

--- client/src/components/Today/Seasonal.js
+++ client/src/components/Today/Seasonal.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-
+import Slider from 'react-slick';
 
 class Seasonal extends Component {
   render() {
+    const settings = {
+      accesibility: true,
+      slidesToShow: 7,
+      slidesToScroll: 7,
+      lazyLoad: true,
+      arrows: true,
+      dots: true,
+    }
     return (
       <div className="Seasonal">
         <h2>What's in Season</h2>
-        {this.props.seasonalProducts.map( (product) => {
-          return (
-            <div className="seasonal-product" key={product.id}>
-              <img src={product.pic_small_url} alt={product.name}/>
-              <p>{product.name}</p>
-            </div>
-          )
-        })}
+        <Slider className="seasonal-slider" {...settings}>
+          {this.props.seasonalProducts.map( (product) => {
+            return (
+              <div className="seasonal-product" key={product.id}>
+                <img src={product.pic_small_url} alt={product.name}/>
+                <p>{product.name}</p>
+              </div>
+            )
+          })}
+        </Slider>
       </div>
     )
   }
