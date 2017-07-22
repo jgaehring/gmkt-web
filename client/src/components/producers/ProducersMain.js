ec9a3f10748591e716dc3b8e2d55610b4d5d6d0b
James Gaehring <gaehj457@gmail.com>
2017-07-22 19:18:43 -0400

    make calls to API for only current Union Sq producers and add object to state

--- client/src/components/producers/ProducersMain.js
+++ client/src/components/producers/ProducersMain.js
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
+import Spinner from 'modules/Spinner'
 import Header from 'modules/Header';
 
 class ProducersMain extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true
+    };
+  }
+
+  fetchProducers() {
+    fetch('/api/v1/producers')
+      .then(resp => resp.json())
+      .then((data) => {
+        const currentProducers = data.producers.filter(producer => {
+          let isCurrent = false;
+          const current = producer.seasons.forEach(season => {
+            const end = new Date(season.end_date);
+            const today = new Date();
+            if (end >= today) {
+              isCurrent = true;
+            }
+          })
+          if (isCurrent) {
+            return producer;
+          }
+          return current;
+        });
+        this.setState({
+          loading: false,
+          currentProducers: currentProducers
+        })
+      });
+  }
+
+  componentDidMount() {
+    this.fetchProducers();
+  }
+
   render() {
-    return (
-      <Header>
-        <h1>Producers Page</h1>
-      </Header>
-    )
+    if (this.state.loading) {
+      return <Spinner />
+    } else {
+      return (
+        <Header>
+          <h1>Producers Page</h1>
+        </Header>
+      )
+
+    }
   }
 }
 
