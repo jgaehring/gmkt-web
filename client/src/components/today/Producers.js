cc4b21e42ffab55b6037ba5eb865aaab200f17de
James Gaehring <gaehj457@gmail.com>
2017-08-09 22:59:16 -0400

    fixing/simplifying/adding flexbox to producer lists in Today

--- client/src/components/today/Producers.js
+++ client/src/components/today/Producers.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import Producer from 'today/Producer';
 import Section from 'modules/Section'
-import './Producers.css'
+import 'today/Producers.css'
+import {Link} from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
+import Thumbnail from 'modules/Thumbnail';
+import getTypeIcon from 'media/product-types/getTypeIcon.js';
+
+function getProducerImage(pic, type) {
+  if (pic === undefined) {
+    return getTypeIcon(type);
+  } else {
+    return pic;
+  }
+}
+
+function Producer(props) {
+  const pic = props.producer.pic_url;
+  const type = props.producer.main_type;
+  const imgURL = getProducerImage(pic, type);
+  return (
+    <Link to={"/producers/" + props.producer.id} className="Producer">
+      <LazyLoad height={"20vw"} offset={500} once>
+        <Thumbnail
+            imgURL={imgURL}
+            altText={props.producer.name}
+            thumbnailText={props.producer.name}
+          />
+      </LazyLoad>
+    </Link>
+  )
+};
 
 function Producers(props) {
-
   if (props.producers.length > 0) {
     return (
       <Section className="Producer-List">
