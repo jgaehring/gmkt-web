f63208f87cdddb7900a95ffe80c75ca20cbbf3a3
James Gaehring <gaehj457@gmail.com>
2017-12-19 17:33:04 -0500

    refactoring Row styles so their more reusable (removing flexbox)

--- client/src/components/products/ProducerPresences.js
+++ client/src/components/products/ProducerPresences.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Section from 'modules/Section';
-import {ProducerRow, RowIcon, RowName, RowDate} from 'modules/ProducerRows'
+import {
+  ProducerRow,
+  RowIcon,
+  RowName,
+  RowDate } from 'modules/ProducerRows'
 
 // Renders a list of all producers who have sold the product in the last week
 export default function ProducerPresences(props) {
   const cutKeys = (p) => p.producer_id + "_" + p.date;
   return (
     <Section>
-      {
-        props.presences.map( p =>
-          <ProducerRow key={cutKeys(p)} id={p.producer_id} >
-            <RowIcon type={p.producerInfo.main_type} />
-            <RowName name={p.producerInfo.name} />
-            <RowDate date={p.date} />
-          </ProducerRow>
-        )
-      }
+      <div className="Producer-List">
+        {
+          props.presences.map( p =>
+            <ProducerRow key={cutKeys(p)} id={p.producer_id} >
+              <RowIcon type={p.producerInfo.main_type} />
+              <RowName name={p.producerInfo.name} />
+              <RowDate date={p.date} />
+            </ProducerRow>
+          )
+        }
+      </div>
     </Section>
   )
 }
